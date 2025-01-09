7157b2aaff8ea5f010373c260f1d141ec0c729f0
HNAVMUR <hnavmur@alumno.upv.es@ldsic-vdi05.upvnet.upv.es>
2025-01-09 09:58:27 +0100

    Añadida la clase Point2D

--- Point2D.h
+++ Point2D.h
@@ -2,31 +2,19 @@
 #define POINT2D_H
 
 #include <iostream>
-#include <cmath>
 
 class Point2D {
 public:
     double x;
     double y;
 
-    Point2D(double x = 0, double y = 0) : x(x), y(y) {}
+    Point2D(double x = 0, double y = 0);
 
-    static double distance(const Point2D &a, const Point2D &b) {
-        return std::sqrt(std::pow(a.x - b.x, 2) + std::pow(a.y - b.y, 2));
-    }
+    static double distance(const Point2D &a, const Point2D &b);
 
-    friend bool operator==(const Point2D &a, const Point2D &b) {
-        return a.x == b.x && a.y == b.y;
-    }
-
-    friend bool operator!=(const Point2D &a, const Point2D &b) {
-        return !(a == b);
-    }
-
-    friend std::ostream& operator<<(std::ostream &out, const Point2D &p) {
-        out << "(" << p.x << ", " << p.y << ")";
-        return out;
-    }
+    friend bool operator==(const Point2D &a, const Point2D &b);
+    friend bool operator!=(const Point2D &a, const Point2D &b);
+    friend std::ostream& operator<<(std::ostream &out, const Point2D &p);
 };
 
 #endif
