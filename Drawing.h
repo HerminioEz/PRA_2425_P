167b5c5546f9c6515a3272c69367ce12db35cdd6
HNAVMUR <hnavmur@alumno.upv.es@ldsic-vdi05.upvnet.upv.es>
2025-01-09 11:11:50 +0100

    Se ha añadido la clase Drawing

---
+++ Drawing.h
@@ -0,0 +1,25 @@
+#ifndef DRAWING_H
+#define DRAWING_H
+
+#include <vector>
+#include "Shape.h"
+#include "Circle.h"
+#include "Square.h"
+
+class Drawing {
+private:
+    std::vector<Shape*> shapes;
+
+public:
+    Drawing();
+    ~Drawing();
+
+    void add_front(Shape* shape);
+    void add_back(Shape* shape);
+    void print_all() const;
+    double get_area_all_circles() const;
+    void move_squares(double incX, double incY);
+};
+
+#endif
+
